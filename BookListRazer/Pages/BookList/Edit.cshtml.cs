7175ca5eb4412999fd397d499bbe191fd6a82714
Mohamed <mohamedhelmoghazy.gmail.com>
2020-08-12 23:10:12 -0600

    Apply Delete Button

--- BookListRazer/Pages/BookList/Edit.cshtml.cs
+++ BookListRazer/Pages/BookList/Edit.cshtml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using BookListRazer.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookListRazer.Pages.BookList
@@ -24,7 +26,25 @@ namespace BookListRazer.Pages.BookList
         public async Task OnGet(int id)
         {
             book = await _db.Book.FindAsync(id);
-            
+
         }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Book.FindAsync(book.Id);
+                BookFromDb.Name = book.Name;
+                BookFromDb.ISBN = book.ISBN;
+                BookFromDb.Author = book.Author;
+
+                await _db.SaveChangesAsync(); // upadate field into database
+                return RedirectToPage("Index");
+
+
+            }
+            return RedirectToPage();
+
+        }
+            
     }
 }
