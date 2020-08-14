80e614f816783facfaca792b79928df198992ba0
Mohamed <mohamedhelmoghazy.gmail.com>
2020-08-13 22:01:51 -0600

    NewTableWorking

--- BookListRazer/wwwroot/js/bookList.js
+++ BookListRazer/wwwroot/js/bookList.js
@@ -13,25 +13,25 @@ function loadDataTable() {
         },
         "columns": [
             { "data": "name", "width": "20%" },
-            { "data": "author", "width": "20%" },
+            { "data": "author", "width":"20%" },
             { "data": "isbn", "width": "20%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return ` <div class = "text-center">
-                    <a href = "/BookList/Edit?id=${data}"class 'btn btn-success text-white' style = 'cursor : pointer ; width : 100 px;'>
+                    <a href = "/BookList/Edit?id=${data}"class='btn btn-success text-white' style = 'cursor : pointer ; width : 70px;'>
                         Edit
                     </a>
 
                     &nbsp;
-                    <a class 'btn btn-danger text-white' style = 'cursor : pointer ; width : 100 px;'>
+                    <a class = 'btn btn-danger text-white' style = 'cursor : pointer sm ; width :70px;'>
                         Delete
                     </a>   
 
                             </div>;
 
 `
-                }, "width": "30%"
+                }, "width": "40%"
 
             }
 
